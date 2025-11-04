95f4c3db4f16fc1bc0096a767875f00fe6e8278f
jaiswal-ankit10 <ankitjaiswalaryan3@gmail.com>
2025-11-05 02:14:46 +0530

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
 
 const App = () => {
   const [showContent, setShowContent] = React.useState(false);
@@ -19,7 +20,7 @@ const App = () => {
       ease: "Expo.easeInOut",
       opacity: 0,
       transformOrigin: "50% 50%",
-      onUpdate: () => {
+      onUpdate: function () {
         if (this.progress() >= 0.9) {
           document.querySelector(".svg").remove();
           setShowContent(true);
@@ -59,6 +60,59 @@ const App = () => {
           />
         </svg>
       </div>
+      {showContent && (
+        <div className="main  w-full h-screen ">
+          <div className="landing relative w-full h-screen bg-black overflow-hidden">
+            <div className="navbar absolute top-0 left-0  z-10 py-10 px-10">
+              <div className="logo flex gap-7">
+                <div className="lines flex flex-col gap-[5px]">
+                  <div className="line1 bg-white w-15 h-2"></div>
+                  <div className="line2 bg-white w-8 h-2"></div>
+                  <div className="line3 bg-white w-5 h-2"></div>
+                </div>
+                <h3 className="navtext text-white text-4xl -mt-2 leading-none">
+                  Rockstar
+                </h3>
+              </div>
+            </div>
+            <div className="imagesdiv">
+              <img
+                className="absolute sky  top-0 left-0 w-full h-full object-cover"
+                src="./sky.png"
+                alt=""
+              />
+              <img
+                className="absolute  bg top-0 left-0 w-full h-full object-cover"
+                src="./bg.png"
+                alt=""
+              />
+              <div className="text text-white flex flex-col gap-3 absolute top-20 left-1/2 -translate-x-1/2">
+                <h1 className="text-[12rem] leading-none -ml-40">grand</h1>
+                <h1 className="text-[12rem] leading-none ml-20">theft</h1>
+                <h1 className="text-[12rem] leading-none -ml-40">auto</h1>
+              </div>
+              <img
+                className="absolute character left-1/2 -bottom-90 -translate-x-1/2  "
+                src="./girlbg.png"
+                alt=""
+              />
+            </div>
+            <div className="bottombar text-white absolute bottom-0 w-full  py-15 px-10 bg-linear-to-t from-black to-transparent">
+              <div className="scrlldown flex gap-4 items-center">
+                <i className="text-4xl ri-arrow-down-line"></i>
+                <h3 className="text-xl font-[Helvetica_Now_Display]">
+                  Scroll Down
+                </h3>
+              </div>
+              <img
+                className="absolute h-[55px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="./ps5.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
